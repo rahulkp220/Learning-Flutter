46c8b9d1476b3d1472ae74b008c1a6e2697ed14a
rahul lakhanpal <rahulkp220@gmail.com>
2018-10-03 02:43:39 +0530

    updated code and image

--- handling_json_app/lib/main.dart
+++ handling_json_app/lib/main.dart
@@ -12,15 +12,16 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final String url = "https://swapi.co/api/starships";
+  final String url = "https://api.github.com/users/rahulkp220";
   var data = [];
 
   Future<String> getData() async {
     var response = await http
         .get(Uri.encodeFull(url), headers: {"Accept": "application/json"});
+
     setState(() {
       var resBody = json.decode(response.body);
-      data = resBody["results"];
+      data.add(resBody);
     });
 
     return "Success";
@@ -29,33 +30,52 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("JSON API"),
-          backgroundColor: Colors.lightBlueAccent,
-        ),
-        body: ListView.builder(
-            itemCount: data == null ? 0 : data.length,
-            itemBuilder: (BuildContext context, int index) {
-              return Container(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: <Widget>[
-                    Card(
-                      child: Container(
-                        padding: EdgeInsets.all(20.00),
-                        child: Text(
-                          data[index]["name"],
-                          style: TextStyle(color: Colors.deepOrangeAccent),
+        home: Scaffold(
+            appBar: AppBar(
+                title: Text("Github API"), backgroundColor: Colors.black),
+            body: ListView.builder(
+                itemCount: data == null ? 0 : data.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Center(
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                        Container(
+                            margin: EdgeInsets.all(30.00),
+                            width: 300.0,
+                            height: 300.0,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                image: DecorationImage(
+                                    fit: BoxFit.fill,
+                                    image: NetworkImage(
+                                        data[index]["avatar_url"])))),
+                        Card(
+                          child: Container(
+                            padding: EdgeInsets.all(20.00),
+                            child: Column(
+                              children: <Widget>[
+                                ListTile(
+                                  leading: Icon(Icons.verified_user),
+                                  title: Text('Name'),
+                                  subtitle: Text(data[index]["name"]),
+                                ),
+                                ListTile(
+                                  leading: Icon(Icons.description),
+                                  title: Text('Description'),
+                                  subtitle: Text(data[index]["bio"]),
+                                ),
+                                ListTile(
+                                  leading: Icon(Icons.web),
+                                  title: Text('Blog'),
+                                  subtitle: Text(data[index]["blog"]),
+                                ),
+                              ],
+                            ),
+                          ),
                         ),
-                      ),
-                    )
-                  ],
-                ),
-              );
-            }),
-      ),
-    );
+                      ]));
+                })));
   }
 
   @override
