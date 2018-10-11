526cb8cfdfbef07dedc405cdde42f7c9ab3c6141
Harshil Agrawal <ghagrawal17@gmail.com>
2018-10-11 13:14:23 +0530

    added comments to main.dart

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -1,13 +1,18 @@
+// import material library
 import "package:flutter/material.dart";
 
+// Run the HelloWorld app
 void main() => runApp(new HelloWorld());
 
+// Create statelesswidget class
 class HelloWorld extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
       home: new Material(
+        // Center the child widget 
         child: new Center(
+          // Text widget displays the required text
           child: new Text("Hello World"),
         ),
       ),
